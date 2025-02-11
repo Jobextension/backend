61e0ef6792c6b44faa7cdf73a6ce596f68a08ddf
jaeyo03 <toyazx@naver.com>
2025-02-12 03:12:07 +0900

    feat : supabase 연결 구현

--- index.js
+++ index.js
@@ -1,64 +1,79 @@
+require('dotenv').config()
 const express = require('express');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
+const { createClient } = require('@supabase/supabase-js');
 
 const app = express()
 const port = 3000
-const {Schema} =  mongoose;
 
 app.use(express.json());
-dotenv.config();
 
-const mongoURI = 'mongodb://localhost:27017/job_extension'; // mydatabase는 원하는 데이터베이스 이름
-mongoose.connect(mongoURI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
 
-const resumeSchema = new Schema({
-  jobCategory : String,
-  question : String,
-  answer : String,
-})
-
-const userSchema = new Schema({
-  id : String,
-  password: String,
-})
-
-const Resume = mongoose.model('user_resumes', resumeSchema);
-const User = mongoose.model('users', userSchema);
-
+// Routing
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.status(200).send('Hello World!')
 })
 
-app.get('/api/user/resume', (req, res) => {
-  try{
-    
-  } catch (e) {
-    
-  }
-})
-
+// 유저 resume post
 app.post('/api/user/resume', async (req, res) => {
   try{
     const jsonData = req.body;
-    console.log(jsonData);
-    // 추후에 키워드 뽑아내는 로직이 여기 추가 되어야 한다
-    await Resume.create(jsonData);
-    res.send('success');
+    console.log(req.header('User-ID'));
+    console.log(jsonData)
+
+    // TODO 추후에 키워드 뽑아내는 로직이 여기 추가 되어야 한다
+
+    const userID = req.header('User-ID');
+    if (!userID) {
+      return res.status(401).send('User-ID header is required.');
+    }
+
+    const { data, error } = await supabase
+      .from('resumes')
+      .insert({
+        user_id : userID,
+        folder_id : jsonData.folderID,
+        job_category : jsonData.jobCategory,
+        question : jsonData.question,
+        answer : jsonData.answer,
+        user_wrote_keywords : jsonData.userWroteKeywords,
+        extracted_keywords : jsonData.extractedKeywords
+      })
+      .select();
+
+    if (error) {
+      console.log(error)
+      return res.status(500).send('Supabase Error');
+    }
+
+    console.log(data)
+    return res.status(200).send('success');
   } catch (e) {
-    res.send('error');
+    return res.status(500).send('error');
   }
 })
 
+// resume search
 app.post('/api/user/resumes/search', async (req, res) => {
   try{
     const jsonData = req.body;
     console.log(jsonData);
-    res.send('success')
+    // TODO 추후에 키워드 뽑아내는 로직이 여기 추가 되어야 한다, keywords 수정 필요
+    const keywords = ["성격"]
+
+    const { data, error } = await supabase
+      .from('resumes')
+      .select()
+      .contains('user_wrote_keywords',keywords)
+
+    if (error) {
+      console.log(error)
+      return res.status(500).send('Supabase Error');
+    }
+    console.log(data)
+    return res.status(200).json(data);
   } catch (e) {
-    res.send('error')
+    return res.status(500).send('error');
   }
 })
 
