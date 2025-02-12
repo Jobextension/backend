648687c858bf41fa97b93884554c093de63a3588
jaeyo03 <toyazx@naver.com>
2025-02-12 22:33:10 +0900

    feat : cors 허용

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 require('dotenv').config()
 const express = require('express');
 const { createClient } = require('@supabase/supabase-js');
+const cors = require("cors");
 
 const app = express()
 const port = 3000
+
+app.use(
+  cors({
+    origin: process.env.CHROME_EXTENSION_URL,
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type, Authorization, User-ID",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
